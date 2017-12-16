490d7be45f2678ebf07b0e83dcbaf3d5b1812791
davidmv74 <davidmv74@gmail.com>
2017-12-15 20:59:09 -0500

    PUshing latest version

--- part_two.py
+++ part_two.py
@@ -16,7 +16,7 @@ import parser
 import meter
 from sklearn.feature_extraction.text import CountVectorizer
 import part_one
-
+import lstm
 
 # DAN MODEL
 class DAN(nn.Module):
@@ -25,7 +25,7 @@ class DAN(nn.Module):
         super(DAN, self).__init__()
 
         embed_dim = 299     # 200 Initial States
-        hidden_dim = embed_dim      # Also 200 hidden states, we could change this as hyper parameter (they use around 250 on paper)
+        hidden_dim = 299      # Also 200 hidden states, we could change this as hyper parameter (they use around 250 on paper)
 
         self.W_input = nn.Linear(embed_dim, hidden_dim)
 
@@ -43,7 +43,7 @@ class Net(nn.Module):
         hidden_dim = 2      # The domain it belongs to
 
         self.W_input = nn.Linear(embed_dim, 2)
-        self.W_hidden = nn.Sigmoid()
+        self.W_hidden = nn.Softmax()
 
     def forward(self, review_features):
         hidden = F.tanh(self.W_input(review_features)) # 200 -> hidden_dim
@@ -52,8 +52,8 @@ class Net(nn.Module):
 
 
 def train_adversarial_model(idToQuestions, embeddings,label_train_data, domain_train_data, label_model, domain_model, lr, wd, epochs, batch_size, num_tests):
-    domain_optimizer = torch.optim.Adam(domain_model.parameters(), lr = lr, weight_decay=wd)
-    label_optimizer = torch.optim.Adam(label_model.parameters(), lr = -lr, weight_decay=wd)
+    domain_optimizer = torch.optim.Adam(domain_model.parameters(), lr = -lr, weight_decay=wd)
+    label_optimizer = torch.optim.Adam(label_model.parameters(), lr = lr, weight_decay=wd)
     for epoch in range(1, epochs+1):
         print("-------------\nEpoch {}:\n".format(epoch))
         result = run_adversarial_epoch(label_train_data, domain_train_data, label_model, domain_model, domain_optimizer, label_optimizer, batch_size)
@@ -61,6 +61,7 @@ def train_adversarial_model(idToQuestions, embeddings,label_train_data, domain_t
         print(" ")
 
         dev_data = parser.get_android_samples('dev.neg.txt', 'dev.pos.txt', embeddings, idToQuestions, num_tests)
+        testing_android_temp(label_model, dev_data)
         testing_android(label_model, dev_data)
 
 
@@ -84,7 +85,7 @@ def run_adversarial_epoch(label_train_data, domain_train_data, label_model, doma
         domains = domain_model.forward(pairInput)
         y = [0]*size
         x = []
-        criterion = nn.MultiMarginLoss()
+        criterion = nn.MultiMarginLoss(margin=0.1)
         for i in range(size):
             sampleEncodings = encodings[i]
             refQ = sampleEncodings[0]
@@ -101,7 +102,7 @@ def run_adversarial_epoch(label_train_data, domain_train_data, label_model, doma
         x = x.view(y.data.size(0), -1)
         label_loss = criterion(x, y)
         domain_loss = criterion(domains, y.long())
-        totalLoss = label_loss-(10**-3)*domain_loss
+        totalLoss = label_loss-(10**-6)*domain_loss
         totalLoss.backward()
         label_optimizer.step()
         domain_optimizer.step()
@@ -112,7 +113,26 @@ def run_adversarial_epoch(label_train_data, domain_train_data, label_model, doma
     avg_loss = np.mean(losses)
     return avg_loss
 
-def testing_android(model, test_data):
+def testing_android_lstm(model, test_data):
+    cos = nn.CosineSimilarity(dim=0, eps=1e-6)
+    auc = meter.AUCMeter()
+    for dev_sample in test_data:
+        target = [x[1] for x in dev_sample[1:]]
+        embeddings = [x[0] for x in dev_sample]
+        refQ = model.forward(Variable(torch.FloatTensor(embeddings[0])))
+        refQ = embeddings[0].numpy().astype(float)
+        #print "sample", refQ
+        refQ = model.forward(Variable(torch.FloatTensor(\
+                            [refQ])))[0]
+        candidates = embeddings[1:]
+        candidatesCosine = []
+        for i in range(len(candidates)):
+            candidateEncoding = model.forward(Variable(torch.FloatTensor(candidates[i])))
+            candidatesCosine.append(cos(refQ, candidateEncoding).data[0])
+        auc.add(np.array(candidatesCosine), np.array(target))
+    print(auc.value(0.05))
+
+def testing_android(model,test_data):
     cos = nn.CosineSimilarity(dim=0, eps=1e-6)
     auc = meter.AUCMeter()
     for dev_sample in test_data:
@@ -126,6 +146,60 @@ def testing_android(model, test_data):
             candidatesCosine.append(cos(refQ, candidateEncoding).data[0])
         auc.add(np.array(candidatesCosine), np.array(target))
     print(auc.value(0.05))
+
+def testing_android_temp(model, test_data):
+    cos = nn.CosineSimilarity(dim=0, eps=1e-6)
+    allRanks = []
+    for dev_sample in test_data:
+        target = [x[1] for x in dev_sample[1:]]
+        embeddings = [x[0] for x in dev_sample]
+        refQ = model.forward(Variable(torch.FloatTensor(embeddings[0])))
+        candidates = embeddings[1:]
+        candidatesCosine = []
+        for i in range(len(candidates)):
+            candidateEncoding = model.forward(Variable(torch.FloatTensor(candidates[i])))
+            candidatesCosine.append((target[i], cos(refQ, candidateEncoding).data[0]))
+        sortedCosines = sorted(candidatesCosine, key = lambda x: x[1], reverse=True)
+        sortedRanks = [cand[0]  for cand in sortedCosines]
+        allRanks.append(sortedRanks)
+    evaluation = Evaluation(allRanks)
+    print("MAP", evaluation.MAP())
+    print("MRR", evaluation.MRR())
+    print("P@1", evaluation.Precision(1))
+
+def unsupervised_test(test_data):
+    cos = nn.CosineSimilarity(dim=0, eps=1e-6)
+    allRanks = []
+    for dev_sample in test_data:
+        target = [x[1] for x in dev_sample[1:]]
+        embeddings = [x[0] for x in dev_sample]
+        refQ = torch.FloatTensor(embeddings[0])
+        candidates = embeddings[1:]
+        candidatesCosine = []
+        for i in range(len(candidates)):
+            candidateEncoding = torch.FloatTensor(candidates[i])
+            candidatesCosine.append((target[i], cos(refQ, candidateEncoding).numpy()))
+        sortedCosines = sorted(candidatesCosine, key = lambda x: x[1], reverse=True)
+        sortedRanks = [cand[0]  for cand in sortedCosines]
+        allRanks.append(sortedRanks)
+    evaluation = Evaluation(allRanks)
+    print("MAP", evaluation.MAP())
+    print("MRR", evaluation.MRR())
+    print("P@1", evaluation.Precision(1))
+    cos = nn.CosineSimilarity(dim=0, eps=1e-6)
+    auc = meter.AUCMeter()
+    for dev_sample in test_data:
+        target = [x[1] for x in dev_sample[1:]]
+        embeddings = [x[0] for x in dev_sample]
+        refQ = torch.FloatTensor(embeddings[0])
+        candidates = embeddings[1:]
+        candidatesCosine = []
+        for i in range(len(candidates)):
+            candidateEncoding = torch.FloatTensor(candidates[i])
+            candidatesCosine.append(cos(refQ, candidateEncoding).numpy()[0])
+        auc.add(np.array(candidatesCosine), np.array(target))
+    print(auc.value(0.05))
+
 
 if __name__ == "__main__":
     # Get all the questions associated to an id from each data set
@@ -144,7 +218,7 @@ if __name__ == "__main__":
     # get all word embeddings
     print("Getting Glove Embeddings...")
     embeddings = parser.get_embeddings('glove.840B.300d.txt', vocabulary)
-
+    #embeddings = parser.get_embeddings('vectors_pruned.200.txt.gz', vocabulary)
     # train
     print("Getting 2000 Train Samples...")
     label_train_data = parser.get_training_vectors(idToQuestionsUbuntu, embeddings)
@@ -154,13 +228,16 @@ if __name__ == "__main__":
     print("Getting Domain Classification Pairs...")
     domain_train_data = parser.get_domain_train_vectors(idToQuestionsUbuntu, idToQuestionsAndroid, embeddings, len(label_train_data))
     print("Getting Development Samples...")
-    dev_data = parser.get_android_samples('dev.neg.txt', 'dev.pos.txt', embeddings, idToQuestionsAndroid, 1000)
+    #dev_data = parser.get_android_samples('dev.neg.txt', 'dev.pos.txt', embeddings, idToQuestionsAndroid, 2000)
     print("HERE")
-    # Part 2 Milestone 1.a)
-    #testing_android(labelModel, dev_data)
+    #inp = 299
+    #h = 240
+    #labelModel = nn.LSTM(input_size=inp, hidden_size=h, num_layers=1, dropout=0.2)
+    # Part 2 Milestone 1.a) Unsupervised running on Android dataset
+    #unsupervised_test(dev_data)
 
     # Part 2 Milestone 1.b)
-    #part_one.train_model(idToQuestionsAndroid, embeddings, label_train_data, labelModel, 0.0001, 0, 1, 40, 1000, True)
+    #part_one.train_model(idToQuestionsAndroid, embeddings, label_train_data, labelModel, 0.001, 0, 5, 80, True)
 
     # Part 2 Milestine 2
-    train_adversarial_model(idToQuestionsAndroid, embeddings, label_train_data, domain_train_data, labelModel, domainModel, 0.001, 0, 3, 40, 1000)
+    train_adversarial_model(idToQuestionsAndroid, embeddings, label_train_data, domain_train_data, labelModel, domainModel, 0.001, 0, 5, 40, 1000)
